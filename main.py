1581eae7b7d3ad6b479f2acbc9da9179fcae1490
Nikita <xanext@mail.ru>
2022-02-06 19:21:06 +0500

    clear file for next steps

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((400, 500))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('blue')
-test_rect = test_surface.get_rect(topright = (200 ,250))
 grassColor = (175, 215, 70)
 gameRunning = True
 
@@ -14,8 +11,6 @@ while gameRunning:
         if event.type == pygame.QUIT:
             gameRunning = False
     screen.fill(grassColor)
-    test_rect.right += 1
-    screen.blit(test_surface, test_rect)
     pygame.display.update()
     clock.tick(60)
 
