361db643fde32c3785457903db6ccfc267aef30a
Nikitaprogartlocal <xanext@mail.ru>
2022-02-14 14:36:23 +0500

    move Vector2 to new file

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
 import pygame, random
 from pygame import Color as palette
+from vector2 import Vector2
 
-class Vector2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def __add__(self, other):
-        if isinstance(other, Vector2):
-            return(Vector2(self.x + other.x, self.y + other.y))
-        elif isinstance(other, tuple):
-            return(Vector2(self.x + other[0], self.y + other[1]))
-        else:
-            global gameRunning
-            print("Can't equlas these objects")
-            exit()
-
-    def __eq__(self, other):
-        if isinstance(other, Vector2):
-            if self.x == other.x and self.y == other.y:
-                return True
-            else:
-                return False
-        elif isinstance(other, tuple):
-            if self.x == other[0] and self.y == other[1]:
-                return True
-            else:
-                return False
-        else:
-            global gameRunning
-            print("Can't match these objects:", type(self), type(other))
-            exit()
 
 cell_size = 40
 
